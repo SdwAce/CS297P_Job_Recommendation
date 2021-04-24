efa41013abf5d21ae69335d0e477e1d3cf96100c
Andrew Yu <andrewey@uci.edu>
2021-04-23 22:12:33 -0700

    added back_end branch with JDBC code

---
+++ back_end/com/company/JobEntry.java
@@ -0,0 +1,47 @@
+package com.company;
+
+public class JobEntry {
+    public String title;
+    public String company;
+    public String estSalary;
+    public String location;
+    public String description;
+
+    //-----------------------SETTERS-----------------------
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public void setEstimatedSalary(String estSalary) {
+        this.estSalary = estSalary;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    //-----------------------GETTERS-----------------------
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getCompany() {
+        return this.company;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+}
---
+++ back_end/com/company/PostgresWithJDBCConnection.java
@@ -0,0 +1,80 @@
+package com.company;
+//package postgresql.jdbc;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostgresWithJDBCConnection {
+    public static final String ClientPassword = "adminuser";
+    public static final String DATABASE_CLIENT = "postgres";
+    public static final String DATABASE_URL = "jdbc:postgresql://127.0.0.1:5432/job_recommender";
+
+    public static String constructSQLKeywordArray(String[] keywords, String surround) {
+        String returnStr = "["; // add open bracket
+        for (int i = 0; i < keywords.length; i++) {
+            returnStr = returnStr + "'" + surround + keywords[i] + surround + "'"; // add keyword surrounded by % and '
+            if (i < keywords.length - 1) {
+                returnStr = returnStr + ","; // add comma, except for last element
+            }
+        }
+        return returnStr + "]"; // add close bracket
+    }
+
+    public static void viewTable(Connection con, List <JobEntry> jobEntryList, String[] keywords) throws SQLException {
+        String keywordSearchQuery = "SELECT * FROM jobs WHERE job_description LIKE ANY (ARRAY" + constructSQLKeywordArray(keywords, "%") + ")";
+        String keywordSortedQuery = "SELECT id, job_title, company, location, job_description, (array_length(x, 1) - 1) AS freq FROM jobs " +
+                                    "CROSS JOIN LATERAL regexp_split_to_array(job_description, '\\m"+ keywords[0] +"\\M') as t(x) " +
+                                    "where job_description like any (array['%"+ keywords[0] +"%']) order by freq desc";
+        try (Statement stmt = con.createStatement()) {
+            ResultSet rs = stmt.executeQuery(keywordSearchQuery);
+            while (rs.next()) {
+                String title = rs.getString("job_title");
+                String company = rs.getString("company");
+                String estSalary = rs.getString("estimated_salary");
+                String location = rs.getString("location");
+                String description = rs.getString("job_description");
+                JobEntry jobEntry = new JobEntry();
+                jobEntry.setTitle(title);
+                jobEntry.setCompany(company);
+                jobEntry.setEstimatedSalary(estSalary);
+                jobEntry.setLocation(location);
+                jobEntry.setDescription(description);
+                jobEntryList.add(jobEntry);
+            }
+
+            // print top 10 entries
+            int i = 0; // TODO delete
+            for (JobEntry jobEntry : jobEntryList) {
+                System.out.print("Title:: " + jobEntry.getTitle());
+                System.out.print(" | Company:: " + jobEntry.getCompany());
+                System.out.print(" | Location:: " + jobEntry.getLocation());
+//                System.out.print(" | Description:: " + jobEntry.getDescription());
+                System.out.println();
+                i++; // TODO delete
+                if (i == 10)
+                    break;
+            }
+        } catch (SQLException e) {
+            System.out.print(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        List <JobEntry> jobEntryList = new ArrayList < > ();
+        String[] keywords = {"Haskell"}; // FOR NOW: keyword search from static array
+        // establishes database connection
+        // auto closes connection and preparedStatement
+        try (Connection conn = DriverManager.getConnection(DATABASE_URL, DATABASE_CLIENT, ClientPassword);) {
+            viewTable(conn, jobEntryList, keywords);
+        } catch (SQLException e) {
+            System.out.print(e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
